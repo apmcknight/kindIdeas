dfac2b80b81ea99fe54290e514d34f752b6d710d
Adam McKnight <adammcknight@me.com>
2018-08-13 18:54:35 -0400

    Added styling and a copyright footer to the page.

--- application.js
+++ application.js
@@ -4,14 +4,14 @@
 var kind = [
     "Smile at a random person! 😀",
     "Open the door for someone when entering a building.",
-    "Pay for the the Coffee or Meal of the person behind you.",
+    "Pay for the the Coffee or Meal \n of the person behind you.",
     "Tell someone they look perfect today!",
     "Give someone a $20 bill and walk away.",
     "Leave a bottle of watter inside your mailbox.",
     "After shoping, help someone put the bags in their car.",
-    "Close your eyes, and scroll through your contacts. Call the person you land on, and tell them 'You're Worth It!' ",
+    "Close your eyes, and scroll through your contacts. Call the person \n you land on, and tell them 'You're Worth It!' ",
     "Hug a member of your family",
-    "While at the checkout, buy a giftcard; Then give it to someone walking in!"]
+    "While at the checkout, buy a giftcard; \n Then give it to someone walking in!"]
 
 
 
